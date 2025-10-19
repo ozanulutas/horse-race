0c9d4b3c6469a0a79d907d0277eb1384b01b3105
ozanulutas <oznulutas@gmail.com>
2025-10-20 02:47:22 +0300

    feat: create header

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { ref } from 'vue'
+import { watchEffect } from 'vue'
+import BaseButton from '@/components/base/BaseButton.vue'
+import { rounds } from '@/constants'
+
+const store = useStore()
+const isStarted = ref(false)
+const hasProgram = computed(() => !!store.state.schedule.schedules.length)
+const isFinished = computed(() => store.state.race.results.length === rounds.length)
+
+watchEffect(() => {
+  if (isFinished) {
+    isStarted.value = false
+  }
+})
+
+const generateProgram = () => store.dispatch('schedule/generateSchedules')
+const toggleStart = () => {
+  if (isStarted.value) {
+    store.dispatch('race/pause')
+  } else {
+    store.dispatch('race/start')
+  }
+
+  isStarted.value = !isStarted.value
+}
+</script>
+
+<template>
+  <header class="app-header">
+    <h1 class="app-title">Horse Racing</h1>
+
+    <div class="buttons-container">
+      <BaseButton @click="generateProgram" :disabled="isStarted && !isFinished"
+        >GENERATE PROGRAM</BaseButton
+      >
+      <BaseButton @click="toggleStart" :disabled="!hasProgram || isFinished"
+        >START / PAUSE</BaseButton
+      >
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.app-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 6px 12px;
+  background-color: $color-apricot;
+  border: 1px solid $color-black;
+}
+
+.app-title {
+  margin: 0;
+}
+
+.buttons-container {
+  display: flex;
+  gap: 6px;
+}
+</style>
