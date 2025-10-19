d2cb382eb2e4a7a90a31dfe49d33efac5d92544e
ozanulutas <oznulutas@gmail.com>
2025-10-20 02:39:26 +0300

    feat: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export * from '@/constants/colors'
+export * from '@/constants/horseNames'
+export * from '@/constants/rounds'
+export * from '@/constants/common'
---
+++ src/constants/rounds.js
@@ -0,0 +1,26 @@
+export const rounds = [
+  {
+    lap: 1,
+    distance: 1200,
+  },
+  {
+    lap: 2,
+    distance: 1400,
+  },
+  {
+    lap: 3,
+    distance: 1600,
+  },
+  {
+    lap: 4,
+    distance: 1800,
+  },
+  {
+    lap: 5,
+    distance: 2000,
+  },
+  {
+    lap: 6,
+    distance: 2200,
+  },
+]
